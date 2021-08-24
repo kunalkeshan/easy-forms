f0393a057bd6ea92ea5ca1a173a77c97d66725ab
kunalkeshan <kunalkeshan12@gmail.com>
2021-08-24 20:16:32 +0530

    Server initialized

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+require("dotenv").config();
+
+app.set("view engine", "ejs");
+app.use(express.json());
+app.use(express.static(__dirname + "/public"));
+app.use(express.urlencoded({extended: false}));
+
+app.get("/", (req, res) => {
+    res.end("Initialized!");
+});
+
+const PORT = process.env.PORT;
+
+app.listen(PORT, (req, res) => {
+    console.log(`Server is running at http://localhost:${PORT}`);
+})
