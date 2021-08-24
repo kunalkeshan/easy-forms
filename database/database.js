4f1779fbee0adff4536c3bc3cf1e3715bcd553a1
kunalkeshan <kunalkeshan12@gmail.com>
2021-08-24 20:28:24 +0530

    Updated File sturcture

---
+++ database/database.js
@@ -0,0 +1,25 @@
+const mysql = require("mysql");
+require("dotenv").config();
+
+// try {
+//   con = mysql.createConnection({
+    
+//       host: process.env.DB_HOST,
+//       user: process.env.DB_USER,
+//       password: process.env.DB_PASSWD,
+//       database: process.env.DB_NAME,
+//     });
+  
+// } catch (error) {
+//   res.send(error)
+// }
+
+const con = mysql.createConnection({
+    host: process.env.DB_HOST_LOCAL,
+    user: process.env.DB_USER_LOCAL,
+    password: "",
+    database: process.env.DB_NAME_LOCAL,
+});
+con.connect();
+
+module.exports = con;
