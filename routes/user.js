15f588546a0985e44dbeb615c77f98d3d05e8af8
kunalkeshan <kunalkeshan12@gmail.com>
2021-09-09 06:58:16 +0530

    Started User profile routes

--- routes/user.js
+++ routes/user.js
@@ -1,4 +1,26 @@
 const Router = require("express").Router();
+const util = require("util");
+const bcrypt = require("bcryptjs");
+const cookieParser = require("cookie-parser");
 
+Router.use(cookieParser());
+
+//Importing Middleware
+const con = require("../database/database");
+const auth = require("../middleware/auth");
+const app_functions = require("../middleware/app_functions");
+
+const query = util.promisify(con.query).bind(con);
+
+//Get User Profile
+Router.get("/profile", auth, async (req, res) => {
+    const user = req.user;
+    user.created_at = app_functions.convertDate(user.created_at);
+});
+
+//Edit User Profile
+Router.post("/profile/edit", auth, async (req, res) => {
+    
+});
 
 module.exports = Router;
