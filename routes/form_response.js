b7dd791682d06ccec83ee9802c5dceb9c4809ce1
Kunal Keshan <kunalkeshan12@gmail.com>
2021-09-21 20:42:01 +0530

    Worked on uploading files, it works, error handling yet to be done

--- routes/form_response.js
+++ routes/form_response.js
@@ -18,9 +18,16 @@ const query = util.promisify(con.query).bind(con);
 Router.post("/form/response/:id", async (req, res) => {
     const formid = req.params.id;
     const {response} = req.body;
+// Expected response to be, form details, sections array, questions array,
+// options array (if any), responses associated will be with the questions
 
+    //Queries
+    const getForm = `SELECT * FROM form_details WHERE formid='${formid}'`;
     try {
-        
+        const Form = await query(getForm);
+        if(Form.length > 0){
+            const {formdetails, sections, questions, options} = response;
+        } else res.status(400).json({msg: "Form does not exist"});
     } catch (error) {
         console.log({formResponseRoute: error});
         res.status(400).json({msg: "An error has Occured"});
--- routes/images.js
+++ routes/images.js
@@ -4,6 +4,8 @@ const util = require("util");
 const cookieParser = require("cookie-parser");
 const shortid = require("shortid");
 const multer = require("multer");
+const fs = require("fs");
+const path = require("path");
 
 Router.use(cookieParser());
 
@@ -15,13 +17,72 @@ const auth = require("../middleware/auth");
 //Connecting Database and sending Queries
 const query = util.promisify(con.query).bind(con);
 
-//upload image route
-Router.post("/image/upload", auth, async (req, res) => {
+const storage = multer.diskStorage({
+    destination: function(req, file, cb){
+        cb(null, 'public/images');
+    },
+    filename: function(req, file, cb){
+        console.log(file);
+        const FILENAME = Date.now() + "_" + file.originalname;
+        if(req.type === "user_image"){
+            req.user.image_path = `/images/user/${FILENAME}`;
+        }
+        cb(null, FILENAME);
+    }
+});
+const upload = multer({storage: storage})
 
+
+Router.get("/image", (req, res) => {
+    res.render("image");
+})
+
+//upload image route
+Router.post("/image/upload", upload.single("image"), async (req, res) => {
+    const imageid = shortid.generate();
+    const {type} = req.body;
+    const {imagePath, userid} = req.user;
+    
+    const uploadImage = "";
+    const updateUser = "";
+
+    switch (type) {
+        case "user_image":
+            setUploadImage = `INSERT INTO images(imageid, image_type, image_path) VALUES ('${imageid}', '${type}', '${imagePath}')`;
+            setUpdateUser = `UPDATE TABLE user_details SET user_image='custom' WHERE userid='${userid}'`;
+            break;
+    
+        default: throw new Error();
+            break;
+    }
+
+    try {
+        const uploadImage = await query(setUploadImage);
+        if(uploadImage.affectedRows > 0){
+            const updateUser = await query(setUpdateUser);
+            if(updateUser.affectedRows > 0){
+                res.status(200).json({msg: "User image updated Successsfully"});
+            } else throw new Error();
+        } else throw new Error();
+    } catch (error) {
+        console.log({uploadImageRoute: error});
+        res.status(400).json({msg: "An error has occured"});
+    }
 });
 
 //delete image route
 Router.post("/image/delete/:id", auth, async (req, res) => {
+    const imageid = shortid.generate();
+    const {type} = req.body;
+
+
+    //user fs.unlink(path, (err) = {if err consle err and return})
+    try {
+        
+    } catch (error) {
+        console.log({deleteImageRoute: error});
+        res.status(400).json({msg: "An error has occured"});
+    }
 
 });
 
