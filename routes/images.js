1c14ce32c4f71fa117bb65ac2594abd72e6022bd
kunalkeshan <kunalkeshan12@gmail.com>
2021-09-22 06:36:32 +0530

    Tried complex uploading, still errors

--- routes/images.js
+++ routes/images.js
@@ -24,9 +24,15 @@ const storage = multer.diskStorage({
     filename: function(req, file, cb){
         console.log(file);
         const FILENAME = Date.now() + "_" + file.originalname;
-        if(req.type === "user_image"){
+        if(req.body.type === "user_image"){
             req.user.image_path = `/images/user/${FILENAME}`;
-        }
+        } else if (req.body.type === "question"){
+            //for user image
+            req.body.image_path = `/images/question/${FILENAME}`;
+        } else if(req.body.type === "response"){
+            //for question and response
+            req.body.imagePathFQR = `/images/response/${FILENAME}`;
+        } else cb( new Error("Provide a valid type"));
         cb(null, FILENAME);
     }
 });
@@ -40,30 +46,50 @@ Router.get("/image", (req, res) => {
 //upload image route
 Router.post("/image/upload", upload.single("image"), async (req, res) => {
     const imageid = shortid.generate();
-    const {type} = req.body;
-    const {imagePath, userid} = req.user;
+    const {type, imagePathFQR} = req.body;
+    console.log(req.body)
+    /*
+        imagePathFQR = for question and response;
+        image_path = for user images
+    */
     
-    const uploadImage = "";
-    const updateUser = "";
-
-    switch (type) {
-        case "user_image":
-            setUploadImage = `INSERT INTO images(imageid, image_type, image_path) VALUES ('${imageid}', '${type}', '${imagePath}')`;
-            setUpdateUser = `UPDATE TABLE user_details SET user_image='custom' WHERE userid='${userid}'`;
-            break;
-    
-        default: throw new Error();
-            break;
-    }
+    let setUploadImage = "";
+    let setUpdateUser = "";
 
     try {
-        const uploadImage = await query(setUploadImage);
-        if(uploadImage.affectedRows > 0){
-            const updateUser = await query(setUpdateUser);
-            if(updateUser.affectedRows > 0){
-                res.status(200).json({msg: "User image updated Successsfully"});
+        if(type === "user_image"){
+            const {userid, image_path} = req.user;
+            //Queries
+            setUploadImage = `INSERT INTO images(imageid, image_type, image_path) VALUES ('${imageid}', '${type}', '${image_path}');`;
+            setUpdateUser = `UPDATE TABLE user_details SET user_image='custom' WHERE userid='${userid}';`;
+
+            const uploadImage = await query(setUploadImage);
+            if(uploadImage.affectedRows > 0){
+                const updateUser = await query(setUpdateUser);
+                if(updateUser.affectedRows > 0){
+                    res.status(200).json({msg: "User image updated Successsfully", image_path});
+                } else throw new Error();
             } else throw new Error();
-        } else throw new Error();
+
+        } else if(type === "question"){
+            //Queries
+            setUploadImage = `INSERT INTO images(imageid, image_type, image_path) VALUES('${imageid}', '${type}', '${imagePathFQR}');`
+
+            const uploadImage = await query(setUploadImage);
+            if(uploadImage.affectedRows > 0){
+                res.status(200).json({msg: "Question Image uploaded successfully", imagePathFQR})
+            } else throw new Error();
+
+        } else if(type === "response"){
+            //Queries
+            setUploadImage = `INSERT INTO images(imageid, image_type, image_path) VALUES('${imageid}', '${type}', '${imagePathFQR}');`
+
+            const uploadImage = await query(setUploadImage);
+            if(uploadImage.affectedRows > 0){
+                res.status(200).json({msg: "Response Image uploaded successfully", imagePathFQR})
+            } else throw new Error();
+
+        } else res.status(400).json({msg: "Provide a valid type!"});
     } catch (error) {
         console.log({uploadImageRoute: error});
         res.status(400).json({msg: "An error has occured"});
