38c66ade9630f957252b60ba91953161176b7996
Kunal Keshan <kunalkeshan12@gmail.com>
2021-10-30 10:44:19 +0530

    DB Update

--- database/database.js
+++ database/database.js
@@ -1,25 +1,30 @@
 const mysql = require("mysql");
 require("dotenv").config();
 
-// try {
-//   con = mysql.createConnection({
+try {
+  con = mysql.createConnection({
     
-//       host: process.env.DB_HOST,
-//       user: process.env.DB_USER,
-//       password: process.env.DB_PASSWD,
-//       database: process.env.DB_NAME,
-//     });
+      host: process.env.DB_HOST,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWD,
+      database: process.env.DB_NAME,
+    });
   
+} catch (error) {
+  res.send(error)
+}
+
+// try {
+//     const con = mysql.createConnection({
+//         host: process.env.DB_HOST_LOCAL,
+//         user: process.env.DB_USER_LOCAL,
+//         password: "",
+//         database: process.env.DB_NAME_LOCAL,
+//     });
 // } catch (error) {
-//   res.send(error)
+//     res.send(error);
 // }
 
-const con = mysql.createConnection({
-    host: process.env.DB_HOST_LOCAL,
-    user: process.env.DB_USER_LOCAL,
-    password: "",
-    database: process.env.DB_NAME_LOCAL,
-});
 con.connect();
 
 module.exports = con;
